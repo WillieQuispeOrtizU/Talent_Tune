a1cc97f677ce16dc5a9699830e9cae8a209ed77d
Slayer13pro <137821783+Slayer13pro@users.noreply.github.com>
2024-09-17 00:47:28 -0500

    Update IUsuarioRepository.java

--- src/main/java/pe/edu/upc/talent_tune/repositories/IUsuarioRepository.java
+++ src/main/java/pe/edu/upc/talent_tune/repositories/IUsuarioRepository.java
@@ -6,4 +6,6 @@ import pe.edu.upc.talent_tune.entities.Usuario;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+  @Query("Select u from Usuario u where u.persona.pais like %:pais%")
+    public List<Usuario> buscarPorPais(@Param("pais") String pais);
 }
