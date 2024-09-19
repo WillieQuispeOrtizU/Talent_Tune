97a08e20851a6193886e2b3cc13a5f1108d12c89
Willie Imanol Quispe Ortiz <u201823814@upc.edu.pe>
2024-09-19 16:22:20 -0500

    Tabla UsuarioBanda

---
+++ src/main/java/pe/edu/upc/talent_tune/entities/UsuarioBanda.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.talent_tune.entities;
+
+public class UsuarioBanda {
+}
--- src/main/java/pe/edu/upc/talent_tune/repositories/IUsuarioRepository.java
+++ src/main/java/pe/edu/upc/talent_tune/repositories/IUsuarioRepository.java
@@ -1,8 +1,12 @@
 package pe.edu.upc.talent_tune.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.talent_tune.entities.Usuario;
+
+import java.util.List;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
